0c0e2b396f440be280b8eb9834b7c155c112a289
AntXx4 <68374972+AntXx4@users.noreply.github.com>
2020-07-16 16:35:14 +0700

    Update adlog.php

--- adlog.php
+++ adlog.php
@@ -1,52 +1,52 @@
-<? php
-// Nama Penulis: Ac3p_Cyb3r
-// Daftar Informasi: 4001 daftar
+<?php
+// Author Name : AntXx4
+// Information List : 4000 list
 
 
-cetak "   
+print "   
 	
- █████╗ ██████╗ ███╗ ███╗██╗███╗ ██╗ ███████╗██╗███╗ ██╗█ █████╗ ███████╗██████╗ 
-██╔══██╗██╔══██╗████╗ ████║██║████╗ ██║ ██╔════╝██║██ ██╗ ██║██╔══██╗██╔════╝██╔══██╗
-███████║██║ ██║██╔████╔██║██║██╔██╗ ██║ █████╗ ██║██╔█ █╗ ██║██║ ██║█████╗ ██████╔╝
-██╔══██║██║ ██║██║╚██╔╝██║██║██║╚██╗██║ ██╔══╝ ██║██║ ╚██╗██║██║ ██║██╔══╝ ██╔══██╗
-██║ ██║██████╔╝██║ ╚═╝ ██║██║██║ ╚████║ ██║ ██║██║ ╚████║ ██████╔╝███████╗██║ ██║
-╚═╝ ╚═╝╚═════╝ ╚═╝ ╚═╝╚═╝╚═╝ ╚═══╝ ╚═╝ ╚═╝╚═╝ ╚═══╝╚═════ ╝ ╚══════╝╚═╝ ╚═╝
+ █████╗ ██████╗ ███╗   ███╗██╗███╗   ██╗    ███████╗██╗███╗   ██╗██████╗ ███████╗██████╗ 
+██╔══██╗██╔══██╗████╗ ████║██║████╗  ██║    ██╔════╝██║████╗  ██║██╔══██╗██╔════╝██╔══██╗
+███████║██║  ██║██╔████╔██║██║██╔██╗ ██║    █████╗  ██║██╔██╗ ██║██║  ██║█████╗  ██████╔╝
+██╔══██║██║  ██║██║╚██╔╝██║██║██║╚██╗██║    ██╔══╝  ██║██║╚██╗██║██║  ██║██╔══╝  ██╔══██╗
+██║  ██║██████╔╝██║ ╚═╝ ██║██║██║ ╚████║    ██║     ██║██║ ╚████║██████╔╝███████╗██║  ██║
+╚═╝  ╚═╝╚═════╝ ╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝    ╚═╝     ╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═╝  ╚═╝
    
-===========================================                                                                             
-  Nama Penulis: Ac3p_Cyb3r
-  Daftar informasi: 4001 daftar
-  Tim Saya: Keamanan Cyber ​​Mouse
-===========================================
-" ;
+=============================================                                                                             
+  Author Name        : AntXx4
+  Information list   : 4000 list
+  My Team            : LightSun
+=============================================
+";
 
-echo  "masukan situs:" ;
-$ target = trim ( fgets ( STDIN ));
-$ list = "mcs_wordlist.txt" ;
-if (! preg_match ( "/ ^ http: \ / \ //" , $ target ) AND! preg_match ( "/ ^ https: \ / \ //" , $ target )) {
-	$ targetnya = "http: // $ target" ;
-} lain {
-	$ targetnya = $ target ;
+echo "masukan site  : ";
+$target = trim(fgets(STDIN));
+$list = "mcs_wordlist.txt";
+if(!preg_match("/^http:\/\//",$target) AND !preg_match("/^https:\/\//",$target)){
+	$targetnya = "http://$target";
+}else{
+	$targetnya = $target;
 }
 
-$ buka = fopen ( "$ list" , "r" );
-$ ukuran = filesize ( "$ list" );
-$ baca = fread ( $ buka , $ ukuran );
-$ daftar = meledak ( "\ r \ n" , $ baca );
+$buka = fopen("$list","r");
+$ukuran = filesize("$list");
+$baca = fread($buka,$ukuran);
+$lists = explode("\r\n",$baca);
 
-foreach ( $ daftar  sebagai  $ login ) {
-	$ log = "$ targetnya / $ login" ;
-	$ ch = curl_init ( "$ log" );
-	curl_setopt ( $ ch , CURLOPT_FOLLOWLOCATION , 1 );
-	curl_setopt ( $ ch , CURLOPT_RETURNTRANSFER , 1 );
-	curl_exec ( $ ch );
-	$ httpcode = curl_getinfo ( $ ch , CURLINFO_HTTP_CODE );
-	curl_close ( $ ch );
-	if ( $ httpcode == 200 ) {
-		 $ handle = fopen ( "result.txt" , "a +" );
-		fwrite ( $ handle , "$ log \ n" );
-		cetak "\ n \ n [" . tanggal ( 'H: m: s' ). "] Mencoba: $ log => Ditemukan \ n" ;
-	} lain {
-		cetak "\ n [" . tanggal ( 'H: m: s' ). "] Mencoba: $ log => tidak di temukan" ;
+foreach($lists as $login){
+	$log = "$targetnya/$login";
+	$ch = curl_init("$log");
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_exec($ch);
+	$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	curl_close($ch);
+	if($httpcode == 200){
+		 $handle = fopen("result.txt", "a+");
+		fwrite($handle, "$log\n");
+		print "\n\n [".date('H:m:s')."] Mencoba : $log => Ditemukan\n";
+	}else{
+		print "\n[".date('H:m:s')."] Mencoba : $log => tidak di temukan";
 	}
 }
   
